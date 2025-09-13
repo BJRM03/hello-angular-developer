a7fd53ff62ba349bcc72e96930366a0e23b9f07f
Billy Jake Ruiz Madrid <u202116401@upc.edu.pe>
2025-09-13 16:05:24 -0500

    feat(developer): add developer entity.

---
+++ src/app/greetings/domain/model/developer.entity.ts
@@ -0,0 +1,21 @@
+export class Developer {
+  private readonly _firstName:string;
+  private readonly _lastName:string;
+
+  constructor(firstName:string, lastName:string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  get firsName(): string{
+    return this._firstName;
+  }
+
+  get lastName(): string{
+    return this._lastName;
+  }
+
+  get fullName(): string{
+    return `${this._firstName} ${this._lastName}`.trim();
+  }
+}
